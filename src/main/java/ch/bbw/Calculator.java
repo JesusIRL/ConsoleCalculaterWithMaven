32b5f2916d396ae3183fa9a0065710cf04e4dd1a
Noobslayer69 <jorin.heer@gmail.com>
2021-06-29 21:32:34 +0200

    Subtaction shall be added

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
     public int summe(int summand1, int summand2){
         return summand1+ summand2;
     }
+    public int differenz(int summand1, int summand2){
+        return summand1- summand2;
+    }
 }
