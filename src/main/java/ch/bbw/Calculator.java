3acc038c242d7f756378a8a632a3806aedeb331a
Noobslayer69 <jorin.heer@gmail.com>
2021-06-29 22:46:35 +0200

    Prepare test for subtraction

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
         return summand1+ summand2;
     }
     public int differenz(int summand1, int summand2){
-        return summand1- summand2;
+       // return summand1- summand2;
+        return 0;
     }
 }
