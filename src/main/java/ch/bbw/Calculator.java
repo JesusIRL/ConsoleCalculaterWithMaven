4934952e5bc880ce923caa085bd16940859eb892
Noobslayer69 <jorin.heer@gmail.com>
2021-06-29 22:49:30 +0200

    test for subtraction shall work

--- src/main/java/ch/bbw/Calculator.java
+++ src/main/java/ch/bbw/Calculator.java
@@ -4,8 +4,7 @@ public class Calculator {
     public int summe(int summand1, int summand2){
         return summand1+ summand2;
     }
-    public int differenz(int summand1, int summand2){
-       // return summand1- summand2;
-        return 0;
+    public int differenz(int value1, int value2){
+        return value1- value2;
     }
 }
