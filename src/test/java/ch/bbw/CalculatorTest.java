aa1372841b92535c539ab867de9deb744471c5fe
Noobslayer69 <jorin.heer@gmail.com>
2021-06-29 23:58:59 +0200

    Tests shall be made

--- src/test/java/ch/bbw/CalculatorTest.java
+++ src/test/java/ch/bbw/CalculatorTest.java
@@ -14,23 +14,24 @@ public class CalculatorTest {
     }
     @Test
     public void testSummeZweiPositiveIsOk(){
-        testee = new Calculator();
         assertTrue(testee.summe(10, 25) == 35);
     }
 
     @Test
-    public void testSubtractionZweiPositiveIsOk(){
-        testee = new Calculator();
+    public void testSubtractionZweiPositiveIsOk(){//Packetproctected macht keinen sinn blyat
         assertTrue(testee.differenz(20, 7) == 13);
     }
     @Test
     public void testSummeZweiNegativeIsOk(){
-        testee = new Calculator();
         assertTrue(testee.summe(-11,-4)== -15);
     }
     @Test
-    public void testSubtractionZweiNegativeIsOk(){
-        testee = new Calculator();
+    public void testSubtractionZweiNegativeIsOk(){//Private macht kein Sinn
         assertTrue(testee.differenz(-13,-5)==-8);
     }
+    @Test(expected = java.lang.ArithmeticException.class)
+    public void testAdditionZahlenBereich(){
+        assertTrue(testee.summe(Integer.MAX_VALUE, 2)!=0);
+    }
+
 }
